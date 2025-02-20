e884eb75991d434e5c8bbe8ba97bffb71a31cad0
90cube <dudgh6418@naver.com>
2025-02-21 01:12:49 +0900

    Fix node mappings

--- node_mappings.py
+++ node_mappings.py
@@ -1,6 +1,7 @@
 # ComfyUI/custom_nodes/MyCustomNodes/node_mappings.py
 
 from .nodes.CB_TestNode import CB_TestNode
+from .nodes.CB_NodeButton import CB_NodeButton
 
 NODE_CLASS_MAPPINGS = {
     "CB_TestNode": CB_TestNode,
