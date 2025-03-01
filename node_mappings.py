d2cb3a5bb3b8fe28fe694fb0b1c65b396965c56e
90cube <dudgh6418@naver.com>
2025-03-01 17:09:33 +0900

    mapping fix

--- node_mappings.py
+++ node_mappings.py
@@ -2,17 +2,21 @@
 
 from .nodes.CB_TestNode import CB_TestNode
 from .nodes.CB_NodeButton import CB_NodeButton
+from .nodes.CB_AdvancedResizer import CB_AdvancedResize
 from .nodes.CB_RatioResizer import CB_RatioResizer
 
 
 NODE_CLASS_MAPPINGS = {
     "CB_TestNode": CB_TestNode,
     "CB_NodeButton": CB_NodeButton,
-    "CB_RatioResizer": CB_Ratio Resizer,
+    "CB_AdvancedResize": CB_AdvancedResize,
+    "CB_RatioResizer": RatioResizer,
+
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CB_TestNode": "CB Test Node",
     "CB_NodeButton": "CB Node Button",
+    "CB_AdvancedResize": "CB Advanced Resize",
     "CB_RatioResizer": "CB Ratio Resizer",
 }
