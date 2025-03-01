4eb9f0959c96dd070bb0ee31749699d2e69d97e4
90cube <dudgh6418@naver.com>
2025-03-01 17:16:48 +0900

    오타 수정

--- node_mappings.py
+++ node_mappings.py
@@ -2,14 +2,14 @@
 
 from .nodes.CB_TestNode import CB_TestNode
 from .nodes.CB_NodeButton import CB_NodeButton
-from .nodes.CB_AdvancedResizer import CB_AdvancedResize
+from .nodes.CB_AdvancedResizer import CB_AdvancedResizer
 from .nodes.CB_RatioResizer import CB_RatioResizer
 
 
 NODE_CLASS_MAPPINGS = {
     "CB_TestNode": CB_TestNode,
     "CB_NodeButton": CB_NodeButton,
-    "CB_AdvancedResize": CB_AdvancedResize,
+    "CB_AdvancedResizer": CB_AdvancedResizer,
     "CB_RatioResizer": RatioResizer,
 
 }
@@ -17,6 +17,6 @@ NODE_CLASS_MAPPINGS = {
 NODE_DISPLAY_NAME_MAPPINGS = {
     "CB_TestNode": "CB Test Node",
     "CB_NodeButton": "CB Node Button",
-    "CB_AdvancedResize": "CB Advanced Resize",
+    "CB_AdvancedResizer": "CB Advanced Resizer",
     "CB_RatioResizer": "CB Ratio Resizer",
 }
