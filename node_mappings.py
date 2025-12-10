02deb10f6a165162b65298a819a793bd0d336d7c
CUBE <dudgh6418@naver.com>
2025-12-10 11:15:48 +0900

    Improve head prompt composition

--- node_mappings.py
+++ node_mappings.py
@@ -4,14 +4,14 @@ from .nodes.CB_TestNode import CB_TestNode
 from .nodes.CB_NodeButton import CB_NodeButton
 from .nodes.CB_AdvancedResizer import CB_AdvancedResizer
 from .nodes.CB_RatioResizer import CB_RatioResizer
-
+from .nodes.head_prompt_node import HeadPromptNode
 
 NODE_CLASS_MAPPINGS = {
     "CB_TestNode": CB_TestNode,
     "CB_NodeButton": CB_NodeButton,
     "CB_AdvancedResizer": CB_AdvancedResizer,
     "CB_RatioResizer": CB_RatioResizer,
-
+    "HeadPromptNode": HeadPromptNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
@@ -19,4 +19,5 @@ NODE_DISPLAY_NAME_MAPPINGS = {
     "CB_NodeButton": "CB Node Button",
     "CB_AdvancedResizer": "CB Advanced Resizer",
     "CB_RatioResizer": "CB Ratio Resizer",
+    "HeadPromptNode": "Head Prompt",
 }
